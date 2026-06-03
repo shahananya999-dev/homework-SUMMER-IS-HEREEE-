c1f4339d371c02e991f01d7c799de925c34645da
shahananya999-dev <shahananya999@gmail.com>
2026-06-03 18:13:06 -0500

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,16 @@
+v = 4
+w = 5
+x = 8
+y = 2
+z = 0
+z = (v+w) * x / y;
+print("Value of (v+x) * x/y is ", z)
+
+name = "Ananya"
+age = 14
+
+if name == "Ananya" or name == "Divija" and age >= 16:
+    print("Hello! Welcome.")
+    
+else:
+    print("Good Bye!!")
